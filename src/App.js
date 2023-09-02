fa12cae085fe8c4e5a6702d9b0b7dd02ae79d296
radwija <115165737+radwija@users.noreply.github.com>
2023-09-03 01:28:57 +0800

    creating components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import { Card } from './components/Card';
+import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
+
 
 function App() {
   return (
     <>
+      <Navbar />
       <Card />
+      <Footer />
     </>
   );
 }
