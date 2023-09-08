425a680433b7762690306a25de46e8d98217eea7
radwija <115165737+radwija@users.noreply.github.com>
2023-09-08 16:15:30 +0800

    registration page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { ProductCard } from './components/ProductCard';
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 import Login from './pages/Login';
+import Registration from './pages/Registration';
 
 
 function App() {
   return (
     <>
       <Navbar />
-      <Login />
+      <Registration />
       <Footer />
     </>
   );
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,21 +1,33 @@
 import React from "react";
-import login from '../assets/images/login.svg'
+import register from '../assets/images/register.svg'
 
-const Login = () => {
+const Registration = () => {
   return (
     <>
       <div className='my-10 sm:flex sm:flex-wrap sm:flex-col md:grid md:grid-cols-12 p-5 '>
         <div className='lg:px-20 md:px-8 py-5 sm:col-span-12 md:col-span-6 hidden sm:hidden md:block lg:block'>
-          <img src={login} alt="Login illustration" />
+          <img src={register} alt="Login illustration" />
         </div>
         <div className='lg:px-20 md:px-8 py-5 sm:col-span-12 md:col-span-6'>
           <div className="p-5 rounded bg-white shadow">
-            <h1 className="text-4xl font-semibold mb-2">Login</h1>
+            <h1 className="text-4xl font-semibold mb-2">Register</h1>
             {/* <div className="alert alert-error">
               <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
               <span>Error! Task failed successfully.</span>
             </div> */}
-            <form action="" className="grid gap-y-3">
+            <form action="" className="grid gap-y-3 mb-4">
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">First name</span>
+                </label>
+                <input type="text" placeholder="First name" className="input input-bordered w-full" />
+              </div>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Last name</span>
+                </label>
+                <input type="text" placeholder="Last name" className="input input-bordered w-full" />
+              </div>
               <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -27,14 +39,11 @@ const Login = () => {
                   <span className="label-text">Password</span>
                 </label>
                 <input type="password" placeholder="Password" className="input input-bordered w-full" />
-                <label className="label">
-                  <span className="label-text-alt">
-                    <a href="">Forgot password?</a>
-                  </span>
-                </label>
               </div>
-              <button type="submit" className="btn btn-primary w-full">Login</button>
+              <button type="submit" className="btn btn-primary w-full">Register</button>
             </form>
+            <hr />
+            <p className="text-center mt-4">Already have an account? <a href="">Login</a></p>
           </div>
         </div>
       </div>
@@ -42,4 +51,4 @@ const Login = () => {
   );
 }
 
-export default Login;
+export default Registration;
