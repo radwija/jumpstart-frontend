62fe6c30b7a6ebe815aa6b609d0c04b46b93eef3
radwija <115165737+radwija@users.noreply.github.com>
2023-09-08 21:50:40 +0800

    md display

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -8,13 +8,13 @@ const ProductDetail = () => {
 
   return (
     <>
-      <div className='py-10 sm:flex sm:flex-wrap sm:flex-col md:grid md:grid-cols-12 px-5 sm:px-5 md:px-5 lg:px-20'>
-        <div className='mx-5 sm:col-span-12 md:col-span-4'>
-          <div className="w-full aspect-square">
+      <div className='py-10 sm:flex sm:flex-wrap sm:flex-col gap-y-10 md:grid md:grid-cols-12 px-5 sm:px-5 md:px-36 lg:px-20'>
+        <div className='mx-5 sm:col-span-12 md:col-span-12 lg:col-span-4'>
+          <div className="w-full aspect-square border rounded">
             <img src="https://thumbs.dreamstime.com/b/vertical-panorama-country-road-9905521.jpg" alt="product thumbnail" className="object-cover w-full h-full rounded" />
           </div>
         </div>
-        <div className='mx-5 sm:col-span-12 md:col-span-5 bg-white'>
+        <div className='mx-5 sm:col-span-12 md:col-span-12 lg:col-span-5 bg-white'>
           <h1 className="text-xl font-semibold">Hot Wheels Fast and Furious Orange Nissan Skyline GT-R BNR34 R34</h1>
           <div className="text-4xl font-bold">$20</div>
 
@@ -26,17 +26,18 @@ const ProductDetail = () => {
 
           <hr className="my-3" />
 
+          <h1 className="text-xl font-semibold">Description</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tristique et egestas quis. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Diam maecenas sed enim ut sem viverra aliquet. Ut sem nulla pharetra diam sit amet. Vitae purus faucibus ornare suspendisse sed nisi. Augue mauris augue neque gravida in fermentum et. At elementum eu facilisis sed odio morbi. Consectetur a erat nam at lectus urna. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Lacus luctus accumsan tortor posuere ac ut consequat semper. Venenatis cras sed felis eget velit aliquet sagittis. Sit amet purus gravida quis blandit turpis cursus. Mattis rhoncus urna neque viverra justo nec ultrices dui sapien. Elementum curabitur vitae nunc sed velit dignissim sodales ut. Aliquam ultrices sagittis orci a. Elit eget gravida cum sociis. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Proin nibh nisl condimentum id venenatis a condimentum vitae.</p>
         </div>
-        <div className='sm:col-span-12 md:col-span-3'>
-          <div className="p-5 rounded bg-white shadow">
+        <div className='sm:col-span-12 md:col-span-12 lg:col-span-3'>
+          <div className="p-5 rounded bg-white border shadow">
             <h1 className="text-xl font-semibold">Order product</h1>
             <div className="mb-3">Stocks: 100</div>
 
             <form action="">
-              <div className="join mb-3">
+              <div className="join w-full mb-3">
                 <button className="btn join-item">-</button>
-                <input type="number" className="join-item w-28 mx-2 border text-center" value={1} min={1} max={100} />
+                <input type="number" className="join-item mx-2 w-full border text-center" value={1} min={1} max={100} />
                 <button className="btn join-item">+</button>
               </div>
               <div className="flex justify-between mb-3">
