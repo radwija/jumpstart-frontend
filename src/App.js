c358214e70b9156ae30e0d40cc9ccc01c7e867bf
radwija <115165737+radwija@users.noreply.github.com>
2023-09-09 12:57:34 +0800

    routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
-import { ProductCard } from './components/ProductCard';
+import { Route, Routes } from "react-router";
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
 import ForgottenPassword from './pages/ForgottenPassword';
 import ResetPassword from './pages/ResetPassword';
-import ProductDetail from './pages/ProductDetail';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Products />
-      <Footer />
+      <div className="App">
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Registration />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/product' element={<ProductDetail />} />
+          <Route path='/find-account' element={<ForgottenPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+        </Routes>
+      </div>
     </>
   );
 }
