68ab1c94af08ac41ade72a884f9016a808df8819
radwija <115165737+radwija@users.noreply.github.com>
2023-09-17 12:10:20 +0800

    fixing increment

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -36,14 +36,21 @@ const ProductDetail = () => {
               <h1 className="text-xl font-semibold">Order product</h1>
               <div className="mb-3">Stocks: 100</div>
 
-              <div className="join w-full mb-3">
-                <button className="btn join-item" onClick={() => setProductNumber((prev) => prev - 1)}>-</button>
-                <input type="number" className="join-item mx-2 w-full border text-center" value={productNumber} onInput={(e) => setProductNumber(Number(e.target.value))} min={1} max={10} />
-                <button className="btn join-item" onClick={() => setProductNumber((prev) => prev + 1)}>+</button>
-              </div>
-
               <form action="">
-                <input type="hidden" className="join-item mx-2 w-full border text-center" value={productNumber} />
+                {/* <input type="hidden" className="join-item mx-2 w-full border text-center" value={productNumber} /> */}
+                <div className="join w-full mb-3">
+                  <button type="button" className="btn join-item" onClick={() => setProductNumber((prev) => prev - 1)}>-</button>
+                  <input
+                    type="number"
+                    className="join-item mx-2 w-full border text-center"
+                    value={productNumber}
+                    onChange={(e) => setProductNumber(Number(e.target.value))}
+                    onInput={(e) => setProductNumber(Number(e.target.value))}
+                    min={1}
+                    max={10}
+                  />
+                  <button type="button" className="btn join-item" onClick={() => setProductNumber((prev) => prev + 1)}>+</button>
+                </div>
                 <div className="flex justify-between mb-3">
                   <div>Total:</div>
                   <div className="text-xl font-semibold">${productNumber * 20}</div>
