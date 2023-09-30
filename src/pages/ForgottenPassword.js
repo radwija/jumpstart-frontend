08536e59e054c401bda1cbf376275eea662e00c2
radwija <115165737+radwija@users.noreply.github.com>
2023-09-30 13:21:39 +0800

    reset password API integration

--- src/pages/ForgottenPassword.js
+++ src/pages/ForgottenPassword.js
@@ -1,24 +1,95 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import forgot_password from '../assets/images/forgot_password.svg'
 import { AlertMessage } from "../components/AlertMessage";
 import Layout from "../components/Layout";
 import { Link } from "react-router-dom";
 import useDocumentTitle from "./useDocumentTitle";
+import { PageHeading } from "../components/PageHeading";
+import { findAccountApi } from "../api/auth-api";
+import { useFormik } from "formik";
+import * as Yup from "yup"
 
 const FindAccountForm = () => {
+  const [alertMessage, setAlertMessage] = useState(null)
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
+
+  const formik = useFormik({
+    initialValues: {
+      email: ""
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .required("Please input your email")
+    }),
+    onSubmit: async (values) => {
+      console.log({ values })
+      findAccountApi(values)
+        .then(res => {
+          setAlertMessage(
+            {
+              messageType: "success",
+              message: res.data.message
+            }
+          )
+        })
+        .catch(error => {
+          if (error?.response?.data) {
+            // error message from backend
+            setAlertMessage(
+              {
+                messageType: "error",
+                message: error?.response?.data.message
+              }
+            )
+          } else if (error) {
+            setAlertMessage(
+              {
+                messageType: "error",
+                message: error.message
+              }
+            )
+          } else {
+            setAlertMessage(
+              {
+                messageType: "error",
+                message: "No response from server"
+              }
+            )
+          }
+        })
+    }
+  })
+
+  const { handleSubmit, handleChange, handleBlur, values, touched, errors } = formik
+
   return (
     <div className="p-5 rounded bg-white border shadow">
-      <h1 className="text-4xl font-semibold mb-2">Find your account</h1>
-      {/* <AlertMessage messageType='error' message='Bro' /> */}
-      <form action="" className="grid gap-y-3 mb-4">
+      <PageHeading headingTitle="Find your account" />
+      {
+        alertMessage &&
+        <AlertMessage messageType={alertMessage.messageType} message={alertMessage.message} />
+      }
+      <form onSubmit={handleSubmit} className="grid gap-y-3 mb-4">
         <div className="form-control w-full">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="Email" className="input input-bordered w-full" />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className="input input-bordered w-full"
+          />
+          {errors.email && touched.email &&
+            <label className="label">
+              <span className="label-text-alt text-red-600">{errors.email}</span>
+            </label>
+          }
         </div>
         <button type="submit" className="btn btn-primary w-full">Login</button>
       </form>
